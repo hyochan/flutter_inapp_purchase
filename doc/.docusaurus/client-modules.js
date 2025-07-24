d0e23e882ebf07e8873b92a3189037199f473775
hyochan <dooboolab@gmail.com>
2025-07-25 06:23:01 +0900

    refactor: rename doc to docs

--- doc/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/hyo/Github/hyochan/flutter_inapp_purchase/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/hyo/Github/hyochan/flutter_inapp_purchase/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/hyo/Github/hyochan/flutter_inapp_purchase/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/hyo/Github/hyochan/flutter_inapp_purchase/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/hyo/Github/hyochan/flutter_inapp_purchase/docs/src/css/custom.css"),
-];
